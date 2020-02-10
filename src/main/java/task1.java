2e68f005c5c8dcf075459def0d4a61948cf6ba0d
shahsevil <you@example.com>
2020-02-10 19:05:47 +0400

    first task

---
+++ src/main/java/task1.java
@@ -0,0 +1,21 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class task1 {
+
+        public static void main(String[] args){
+            //int a,b;
+            // int c=a+b;
+            InputStream in = System.in;
+            Scanner scanner = new Scanner(in);
+            System.out.println("Enter first number:");
+            int a= scanner.nextInt();
+            System.out.println("Enter second number:");
+
+            int b= scanner.nextInt();
+            int c=a+b;
+
+            System.out.printf("Sum of two numbers is:%d\n",c);
+
+        }
+    }
