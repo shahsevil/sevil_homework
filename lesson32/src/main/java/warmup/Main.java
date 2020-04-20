0d31c503309cc6e2bdf20e157181b75e86239f68
shahsevil <you@example.com>
2020-04-20 12:51:20 +0400

    lesson32

--- lesson32/src/main/java/warmup/Main.java
+++ lesson32/src/main/java/warmup/Main.java
@@ -1,26 +1,33 @@
-//package warmup;
-//
-//import java.util.Arrays;
-//
-//public class Main {
-//    private static String dataOrdered(int R, int C, int[][] a) {
-//        String string = Arrays.deepToString(a).replaceAll("],", " ");
-//        string.replaceAll("[,", " ");
-//
-//        return string;
-//    }
-//
-//    public static void main(String[] args) {
-//        int[][]a =
-//                {
-//                        {  1,  2,  3 },
-//                        {  5,  6,  7 },
-//                        {  9, 10, 11 },
-//                        { 13, 14, 15 },
-//                        { 17, 18, 19 },
-//                        { 21, 22, 23 },
-//                };
-//
-//        System.out.println(dataOrdered(a.length, a[0].length, a));
-//    }
-//}
+package warmup;
+
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    private static String dataOrdered(int R, int C, int[][] a) {
+        return IntStream.range(0, R*C).map(idx -> {
+            int row = idx / C;
+            int delta = idx - row * C;
+            int col = (row&1)==0 ? delta : C-1-delta;
+            return a[row][col];
+        })
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(" "));
+
+    }
+
+    public static void main(String[] args) {
+        int[][]a =
+                {
+                        {  1,  2,  3 },
+                        {  5,  6,  7 },
+                        {  9, 10, 11 },
+                        { 13, 14, 15 },
+                        { 17, 18, 19 },
+                        { 21, 22, 23 },
+                };
+
+        System.out.println(dataOrdered(a.length, a[0].length, a));
+    }
+}
